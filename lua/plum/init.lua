c26438e9de0e92ea89bc5ca061d6268e3fdbaf11
9yokuro <xzstd099@protonmail.com>
2024-02-25 23:00:15 +0900

    feat: add lazy load

--- lua/plum/init.lua
+++ lua/plum/init.lua
@@ -44,7 +44,16 @@ function M.clone_repository(plugin_dir, plugin)
 end
 
 function M.add_plugin(plugin)
-	vim.cmd.packadd(M.get_file_name(plugin))
+	local group = "plum"
+
+	vim.api.nvim_create_augroup(group, {})
+
+	vim.api.nvim_create_autocmd("VimEnter", {
+		group = group,
+		callback = function()
+			vim.cmd.packadd(M.get_file_name(plugin))
+		end,
+	})
 end
 
 function M.remove_plugins(plugins)
